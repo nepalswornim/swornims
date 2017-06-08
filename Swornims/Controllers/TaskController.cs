e8db1931c4edf2a2e76bb185e233f65b71a103fa
Swornim Nepal <swornim.nepal@braindigit.com>
2017-06-08 12:44:51 +0545

    Initial Commit

---
+++ Swornims/Controllers/TaskController.cs
@@ -0,0 +1,68 @@
+﻿using Swornims.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+namespace Swornims.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Task
+        public ActionResult Index()
+        {
+            sDBSEntities dbOBj = new sDBSEntities();
+            var tasks = dbOBj.Tasks;
+            List<TaskModel> tListObj = new List<TaskModel>();
+            foreach (Task item in tasks)
+            {
+                TaskModel t = new TaskModel();
+                t.ID = item.ID;
+                t.Name = item.Name;
+                t.Description = item.Description;
+                t.DateCreated = item.DateCreated;
+                t.DateUpdated = item.DateUpdated;
+                tListObj.Add(t);
+            }
+            return View(tListObj);
+        }
+        [HttpPost]
+        public void CreateTask(TaskModel tmdl)
+        {
+            Task t = new Task();
+            t.Name = tmdl.Name;
+            t.Description = tmdl.Description;
+            t.DateCreated = DateTime.Now;
+            sDBSEntities doBj = new sDBSEntities();
+            doBj.Tasks.Add(t);
+            doBj.SaveChanges();
+        }
+        [HttpPost]
+        public void UpdateTask(TaskModel tmdl)
+        {
+            sDBSEntities dbOBj = new sDBSEntities();
+            Task tObj = dbOBj.Tasks.Single(x => x.ID == tmdl.ID);
+            tObj.Name = tmdl.Name;
+            tObj.Description = tmdl.Description;
+            tObj.DateUpdated = DateTime.Now;
+            dbOBj.SaveChanges();
+        }
+        public JsonResult GetTaskById(int id)
+        {
+            sDBSEntities dbOBj = new sDBSEntities();
+            Task t = dbOBj.Tasks.FirstOrDefault(x => x.ID == id);
+            TaskModel tRetMdl = new TaskModel();
+            tRetMdl.ID = t.ID;
+            tRetMdl.Name = t.Name;
+            tRetMdl.Description = t.Description;
+            return  Json(tRetMdl, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public void DeleteTask(int id)
+        {
+            sDBSEntities dbOBj = new sDBSEntities();
+            Task t = dbOBj.Tasks.FirstOrDefault(x => x.ID == id);
+            dbOBj.Tasks.Remove(t);
+            dbOBj.SaveChanges();
+        }
+    }
+}
---
+++ Swornims/Scripts/Tasks.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function () {
+    var form = $("#form1").validate({
+        rules: {
+            txtName: {
+                required: true,
+            },
+            txtDesc: {
+                required: true,
+            }
+        },
+        messages: {
+            txtName: "Task name is required.",
+            txtDesc: "Task Description is required."
+        }
+    });
+    function clearAll() {
+        $("#hdnId").val("")
+        $("#txtDesc").val("");
+        $("#txtName").val("");
+        form.resetForm()
+    }
+    $("#btnCreate").on("click", function () {
+        form.resetForm()
+        $("#mdltitle").text("Create a Task");
+        $("#taskModal").modal("show");
+        clearAll();
+    })
+    $("#btnSave").on("click", function () {
+        if (form.form()) {
+            if ($("#hdnId").val() == "") {
+                $("#taskModal").modal("hide");
+                var tmdl = {
+                    Name: $("#txtName").val(),
+                    Description: $("#txtDesc").val()
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: '/Task/CreateTask',
+                    data: tmdl,
+                    cache: false,
+                    success: function (result) {
+                        clearAll();
+                        alert("Added Successfully");
+                        location.reload();
+                    },
+                    error: function () {
+                        alert('Err');
+                    }
+                });
+            }
+            else {
+                $("#taskModal").modal("hide");
+                var tmdl = {
+                    ID: $("#hdnId").val(),
+                    Name: $("#txtName").val(),
+                    Description: $("#txtDesc").val()
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: '/Task/UpdateTask',
+                    data: tmdl,
+                    cache: false,
+                    success: function (result) {
+                        clearAll();
+                        alert("Updated Successfully");
+                        location.reload();
+                    },
+                    error: function () {
+                        alert('Err');
+                    }
+                });
+            }
+        }
+    });
+    $(".btn_Edit").on("click", function () {
+        clearAll();
+        $("#mdltitle").text("Edit Task");
+        var id = $(this).parent().find("input").attr("id");
+        $.ajax({
+            type: 'GET',
+            url: '/Task/GetTaskById',
+            data: { id: id, },
+            cache: false,
+            success: function (result) {
+                $("#taskModal").modal("show");
+                $("#hdnId").val(result.ID);
+                $("#txtName").val(result.Name);
+                $("#txtDesc").val(result.Description);
+            },
+            error: function () {
+                alert('Err');
+            }
+        });
+    })
+    $(".btn_Delete").on("click", function () {
+
+        var r = confirm("Are you sure you want to delete this Task ?");
+        if (r == true) {
+            deleteTask($(this).parent().find("input").attr("id"));
+        }
+    });
+    function deleteTask(id) {
+        $.ajax({
+            type: 'POST',
+            url: '/Task/DeleteTask',
+            data: { id: id },
+            cache: false,
+            success: function (result) {
+                clearAll();
+                alert("Deleted Successfully");
+                location.reload();
+            },
+            error: function () {
+                alert('Err');
+            }
+        });
+
+    }
+});
